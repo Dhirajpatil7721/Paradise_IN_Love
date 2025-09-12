b3b627aeb1881c2823fd016523014a15f5b5b4be
Dhiraj Patil <151619876+Dhirajpatil7721@users.noreply.github.com>
2025-09-12 16:07:44 +0530

    Update category.route.js

--- Paradise_IN_Love_Backend/route/category.route.js
+++ Paradise_IN_Love_Backend/route/category.route.js
@@ -5,9 +5,9 @@ import { admin } from '../middleware/Admin.js'
 
 const categoryRouter = Router()
 
-categoryRouter.post("/add-category",auth,admin,AddCategoryController)
-categoryRouter.get("/get-category",auth,getCategoryController)
-categoryRouter.put("/update-category",auth,admin,updateCategoryController)
-categoryRouter.delete("/delete-category",auth,admin,deleteCategoryController)
+categoryRouter.post("/add-category",admin,AddCategoryController)
+categoryRouter.get("/get-category",getCategoryController)
+categoryRouter.put("/update-category",admin,updateCategoryController)
+categoryRouter.delete("/delete-category",admin,deleteCategoryController)
 
 export default categoryRouter;
